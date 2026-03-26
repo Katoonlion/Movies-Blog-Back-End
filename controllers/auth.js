cb717afe8b987102913fd0cc5b7912233714e449
Katoon <katoonlion@gmail.com>
2026-03-26 10:14:10 -0400

    Update backend

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,6 +9,7 @@ const User = require('../models/user');
 
 const saltRounds = 12;
 
+// SIGN-UP
 router.post('/sign-up', async (req, res) => {
   try {
     const userInDatabase = await User.findOne({ username: req.body.username });
